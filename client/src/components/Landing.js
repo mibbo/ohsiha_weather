f1d42a59b84c039d6f4e09736e6e5eb3a8d72b31
Tommi Mikkola <tommi.mikkola@tuni.fi>
2020-03-13 16:10:56 +0200

    implement two cards that will weather and apartment temperature data in the future

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react'
 
+
 class Landing extends Component {
    render() {
       return (
-         <div className="container">
-            <div className="jumbotron mt-5">
-               <div className="col-sm-8 mx-auto">
-                  <h1 className="text-center">WELCOME</h1>
-               </div>
-            </div>
+         <div className="cards">
+            <section class="card card--weather">
+               <header>
+                  <h1 className="text-center">Weather</h1>
+               </header>
+               <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+               </ul>
+               <button></button>
+            </section>
+            <section class="card card--egain">
+               <header>
+                  <h1 className="text-center">egain</h1>
+               </header>
+               <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+               </ul>
+               <button></button>
+            </section>
          </div>
       )
+
    }
 }
 
