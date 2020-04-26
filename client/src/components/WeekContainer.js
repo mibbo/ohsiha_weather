f8479efe23c0ddf1a1c9d82c576f78205f76ba13
Tommi Mikkola <tommi.mikkola@tuni.fi>
2020-04-26 11:28:35 +0300

    implement realtime data visualization for dailycards (updates every 6 hours)

--- client/src/components/WeekContainer.js
+++ client/src/components/WeekContainer.js
@@ -6,28 +6,33 @@ class WeekContainer extends React.Component {
    state = {
       fullData: [],
       dailyData: [],
-      location: ''
-
+      location: '',
    }
 
    componentDidMount = () => {
       this.setState({ location: this.props.location });
-      console.log(this.props.location);   //EI TOMI !=?!?!
-
       const weatherURL =
          `http://api.openweathermap.org/data/2.5/forecast?q=Tampere&units=metric&appid=0cb470bd4094e6bdd06e699372db26a4`
 
-      // let url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + this.state.latitude + '&lon=' + this.state.longitude + '&units=metric&appid=0cb470bd4094e6bdd06e699372db26a4';
+      const getQuotes = () => {
+         fetch(weatherURL)
+            .then(res => res.json())
+            .then(data => {
+               const dailyData = data.list.filter(reading => reading.dt_txt.includes("12:00:00"))
+               this.setState({
+                  fullData: data.list,
+                  dailyData: dailyData
+               }, () => console.log(this.state))
+            })
+      };
+      // fetch data when component mounts
+      getQuotes();
+      //fetches data every 6 hours
+      this._interval = window.setInterval(getQuotes, 21600000);
+   }
 
-      fetch(weatherURL)
-         .then(res => res.json())
-         .then(data => {
-            const dailyData = data.list.filter(reading => reading.dt_txt.includes("12:00:00"))
-            this.setState({
-               fullData: data.list,
-               dailyData: dailyData
-            }, () => console.log(this.state))
-         })
+   componentWillUnmount() {
+      this._interval && window.clearInterval(this._interval);
    }
 
    formatDayCards = () => {
