272d7e7ca7442509927b3d59a82aaf19f4ce42b1
Tommi Mikkola <tommi.mikkola@tuni.fi>
2020-04-27 14:59:24 +0300

    fix json web token expiration

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -56,7 +56,7 @@ module.exports = {
                      username: user.username
                   }
                   let token = jwt.sign(payload, process.env.SECRET_KEY, {
-                     expiresIn: 1440000 //1440
+                     // expiresIn: 1440000 //1440
                   })
                   res.send(token)
                   console.log("token");
